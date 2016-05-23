d2e64f286ecd993e6bc0d588626fc25a5ef88c21
Brandon Attala <brandonattala@brandon-attala-macbook-pro.local>
2016-05-23 10:19:20 -0400

    Initial Commit

---
+++ AppleTV_TwitterApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppleTV_TwitterApp
+//
+//  Created by Brandon Attala on 5/23/16.
+//  Copyright © 2016 Brandon Attala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
